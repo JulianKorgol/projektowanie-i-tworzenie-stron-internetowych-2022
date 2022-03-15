84f191262e564e4d7a51f936228b2d4759e7b6b2
Julian Korgol <kontakt@juliankorgol.com>
2022-03-15 21:31:15 +0100

    Projekt 13 (losowanie uczennicy/ucznia) na 2022-03-16

--- Projekt13/script.js
+++ Projekt13/script.js
@@ -1,10 +1,33 @@
-function start() {
-    console.log('Test');
-    var students = [];
-    var list = document.getElementById('list').children;
+var presents = [];
 
+
+function start() {
+    var list = document.getElementById('list').children;
+    var los = 0;
+
+    part2(list)
+}
+
+function part2(list) {
     for (i = 0; i < list.length; i++) {
-        console.dir(list[i].children[0].checked)
-        console.dir(i + '. ' + list[i].children[1].innerText)
+        if (list[i].children[0].checked) {
+            presents.push(list[i].children[1].innerText);
+        }
+    }
+    logosus(presents);
+    setTimeout(logosus(presents), 1000)
+}
+
+function logosus() {
+    while (presents.length > 1) {
+        los = Math.round(Math.random());
+        if (los === 0) {
+            presents = presents.splice(0, Math.floor((presents.length)/2));
+        } else {
+            presents = presents.splice(Math.ceil((presents.length)/2), presents.length);
+        }
+        answer = presents.join();
+        document.getElementById('answer').innerHTML = answer;
+        console.log(presents);
     }
 }
